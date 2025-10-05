4d916ad7d188e0613fb3f9cff920225394b8cdf6
rian <chokchai_ph@cmu.ac.th>
2025-10-06 00:28:01 +0700

    as

---
+++ src/features/Product/AddForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function AddForm({ addProduct }) {
+  const [name, setName] = useState('');
+  const [imageURL, setImageURL] = useState('');
+  const [type, setType] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addProduct({ name, type, imageURL });
+  };
+
+  return (
+    <div>
+      <h1>Add Product</h1>
+      <form id="create-form" onSubmit={handleSubmit} style={{ marginTop: '20px' }}>
+        <div className="input-group">
+          <label htmlFor="name">Name</label>
+          <input name="name" type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="imageURL">Image URL</label>
+          <input name="imageURL" type="text" id="imageURL" value={imageURL} onChange={(e) => setImageURL(e.target.value)} />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="type">Type</label>
+          <input name="type" type="text" id="type" value={type} onChange={(e) => setType(e.target.value)} />
+        </div>
+
+        <button type="submit">Add product</button>
+      </form>
+    </div>
+  );
+}
+
+AddForm.propTypes = {
+  addProduct: PropTypes.func.isRequired
+};
+
+export default AddForm;
